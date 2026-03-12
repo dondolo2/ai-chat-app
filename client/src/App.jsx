f71d9117f0cc4905ff3968b45255c9a963285544
Mosa <dondolomosa@gmail.com>
2026-03-13 00:10:48 +0200

    Added button inside the form, a border separator and an overflow scrollbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1 className='text-center font-bold text-4xl'>Gemini AI Chat App</h1>
+      <h1 className='text-center font-bold text-4xl p-4'>Gemini AI Chat App</h1>
 
-      <div className="max-w-[1320px] mx-auto grid grid-cols-[25%_auto] gap-5">
+      <div className="max-w-[1320px] border-1 mx-auto grid grid-cols-[25%_auto] gap-5 p-5">
         <form action="" className="shadow-lg">
-          <textarea name="" id="" className='w-[100%] h-[200px] border-1'></textarea>
+          <textarea name="" id="" className='w-[100%] h-[200px] border-1 p-3'></textarea>
+          <button className="bg-[#111115] text-white w-[100%] py-2">Create content</button>
         </form>
+
+        <div className="border-l-1 border-[#ccc]">
+          <div className="h-[300px] overflow-scroll p-3"></div>
+        </div>
       </div>
     </>
   )
