547d5588031fcff088d1574f96fa74168b15eaa1
Mosa <dondolomosa@gmail.com>
2026-03-14 21:54:27 +0200

    Set the target value of the textarea to question variable

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,6 @@
+import { useState } from 'react'
 import './App.css'
+import axios from 'axios'
 
 function App() {
 
@@ -7,8 +9,8 @@ function App() {
       <h1 className='text-center font-bold text-4xl p-4'>Gemini AI Chat App</h1>
 
       <div className="max-w-[1320px] border-1 mx-auto grid grid-cols-[25%_auto] gap-5 p-5">
-        <form action="" className="shadow-lg">
-          <textarea name="" id="" className='w-[100%] h-[200px] border-1 p-3'></textarea>
+        <form onSubmit={handleSubmit} action="" className="shadow-lg">
+          <textarea value={question} onChange={(e)=>setQuestion(e.target.value)} name="" id="" className='w-[100%] h-[200px] border-1 p-3'></textarea>
           <button className="bg-[#111115] text-white w-[100%] py-2">Create content</button>
         </form>
 
